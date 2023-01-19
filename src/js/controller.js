86e10967adb8a3404eebd714e6a9431092096cdf
Itzel <104706728+Itzelmariana@users.noreply.github.com>
2023-01-19 11:41:00 -0600

    Create controller.js

---
+++ src/js/controller.js
@@ -0,0 +1,39 @@
+import * as model from './model.js';
+import recipeView from './views/recipeViews.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+async function controlRecipes() {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
+    recipeView.renderSpinner();
+
+    //1) Loading recipe
+    await model.loadRecipe(id);
+
+    //2) Reder recipe view
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
