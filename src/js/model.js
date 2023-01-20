9c4f9b89cd5a4212363dd14649aaf653c417f8f7
Itzel <104706728+Itzelmariana@users.noreply.github.com>
2023-01-20 15:11:14 -0600

    Create model.js

---
+++ src/js/model.js
@@ -0,0 +1,53 @@
+import { async } from 'regenerator-runtime';
+import { API_URL } from './config';
+import { getJSON } from './helpers';
+
+export const state = {
+  recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
+};
+
+export async function loadRecipe(id) {
+  try {
+    const data = await getJSON(`${API_URL}${id}`);
+
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.sourceUrl,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      ingredients: recipe.ingredients,
+      cookingTime: recipe.cooking_time,
+    };
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+export async function loadSearchResults(query) {
+  try {
+    state.search.query = query;
+
+    const data = await getJSON(`${API_URL}?search=${query}`);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
