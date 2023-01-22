8580d1f082e1717ca51cdc5ee680378d571f9c32
Itzel <104706728+Itzelmariana@users.noreply.github.com>
2023-01-22 14:41:35 -0600

    Update controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,7 +1,11 @@
 import * as model from './model.js';
+import { MODAL_CLOSE_SEC } from './config.js';
 import recipeView from './views/recipeViews.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -10,12 +14,11 @@ async function controlRecipes() {
   try {
     const id = window.location.hash.slice(1);
     if (!id) return;
-
+    resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
     recipeView.renderSpinner();
-
     //1) Loading recipe
     await model.loadRecipe(id);
-
     //2) Reder recipe view
     recipeView.render(model.state.recipe);
   } catch (err) {
@@ -26,21 +29,66 @@ async function controlRecipes() {
 async function controlSearchResults() {
   try {
     resultsView.renderSpinner();
-
     const query = searchView.getQuery();
     if (!query) return;
-
     await model.loadSearchResults(query);
-
-    console.log(model.state.search.results);
-    resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 }
 
+function controlPagination(goToPage) {
+  resultsView.render(model.getSearchResultsPage(goToPage));
+  paginationView.render(model.state.search);
+}
+
+function controlServings(newServings) {
+  model.updateServings(newServings);
+  recipeView.update(model.state.recipe);
+}
+
+function controlAddBookmark() {
+  if (!model.state.recipe.bookmarked) {
+    model.addBookmark(model.state.recipe);
+  } else {
+    model.deleteBookmark(model.state.recipe.id);
+  }
+  recipeView.update(model.state.recipe);
+  bookmarksView.render(model.state.bookmarks);
+}
+
+function controlBookmark() {
+  bookmarksView.render(model.state.bookmarks);
+}
+
+async function controlAddRecipe(newRecipe) {
+  try {
+    addRecipeView.renderSpinner();
+    await model.uploadRecipe(newRecipe);
+    recipeView.render(model.state.recipe);
+    addRecipeView.renderMessage();
+
+    bookmarksView.render(model.state.bookmarks);
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
+
+    setTimeout(function () {
+      addRecipeView.toggleWindow();
+    }, MODAL_CLOSE_SEC * 1000);
+  } catch (err) {
+    console.error(err);
+    addRecipeView.renderError(err.message);
+  }
+}
+
 function init() {
+  bookmarksView.addHandlerRender(controlBookmark);
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+  recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 }
 init();
