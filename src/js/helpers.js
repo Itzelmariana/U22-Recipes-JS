226ee13559461f5c0757071bce223d9ed6e932cf
Itzel <104706728+Itzelmariana@users.noreply.github.com>
2023-01-24 23:36:33 -0600

    Update helpers.js

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -20,7 +20,7 @@ export const AJAX = async function (url, uploadData = undefined) {
         })
       : fetch(url);
 
-    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
+    const res = await Promise.resolve(fetchPro);
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
