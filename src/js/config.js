2e1ecd9205aba34f1f70ea0cffaea32cf1bc1f24
Itzel <104706728+Itzelmariana@users.noreply.github.com>
2023-01-24 23:56:00 -0600

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const API_URL = `http://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 50;
 export const RES_PER_PAGE = 10;
 export const KEY = '3237931b-5d84-4aa7-b816-12fd8a118116';
